4c87da88e053f44bd3603829dd4a52fc9a8a70a7
Teana Zhong <Teanawk@gmail.com>
2022-10-29 09:49:50 -0400

    commit tree ba8a9f116d667c9cbf4916a739f64e47d507ca9a

--- control_flow.rb
+++ control_flow.rb
@@ -1,13 +1,37 @@
 def admin_login(username, password)
   # your code here
+  if username == "admin" || username == "ADMIN" && password == "12345"
+    "Access granted"
+
+  else "Access denied"
+  end
+
 end
 
 def hows_the_weather(temperature)
   # your code here
+  response =  if temperature < 40 
+    "brisk"
+  elsif temperature >= 40 && temperature<= 65
+    "little chilly"
+  elsif temperature > 85 
+    "too dang hot"
+  else "perfect"
+
+  "It's #{response} out there!"
 end
 
 def fizzbuzz(num)
   # your code here
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elseif num % 5 == 0
+    "Buzz"
+  else
+    num
+  end
 end
 
 def calculator(operation, num1, num2)
